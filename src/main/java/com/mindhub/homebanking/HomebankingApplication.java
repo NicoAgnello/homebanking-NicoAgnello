291d0b3de56231318ac1d5ec7c3e1b6c855d082f
Nicolas Agnello <nikoagnello1@gmail.com>
2023-03-12 21:00:46 -0300

    add postgress and shadow aside

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -21,81 +21,81 @@ import static com.mindhub.homebanking.utils.Utilities.*;
 @SpringBootApplication
 public class HomebankingApplication {
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+//	@Autowired
+//	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
-	@Bean
-	public CommandLineRunner initData(ClientRepository clientRepository,
-									  AccountRepository accountRepository,
-									  TransactionRepository transactionRepository,
-									  LoanRepository loanRepository,
-									  ClientLoanRepository clientLoanRepository,
-									  CardRepository cardRepository){
-		return args -> {
-			Client client1 = new Client("Melba", "Morel", "melba@mindhub.com", passwordEncoder.encode("melba123"));
-			Client client2 = new Client("Nicolas", "Agnello", "nicoagnello@mindhub.com",  passwordEncoder.encode("nico123"));
-			Client client3 = new Client("admin", "admin", "admin@mindhub.com",  passwordEncoder.encode("admin"));
-
-			Account account1 = new Account("VIN001", LocalDateTime.now(), 5000);
-			Account account2 = new Account("VIN002", LocalDateTime.now().plusDays(1), 7500 );
-			Account account3 = new Account("VIN003", LocalDateTime.now().plusMonths(1), 10000);
-
-			Transaction transaction1 = new Transaction(LocalDateTime.now(), -2000.12, TransactionType.DEBIT, "First transaction");
-			Transaction transaction2 = new Transaction(LocalDateTime.now(), 3000, TransactionType.CREDIT, "Second transaction");
-			Transaction transaction3 = new Transaction(LocalDateTime.now(), 1000.5, TransactionType.CREDIT, "Third transaction");
-			Transaction transaction4 = new Transaction(LocalDateTime.now(), -500, TransactionType.DEBIT, "Fourth transaction");
-
-			Loan mortgage = new Loan("Mortgage", 500000, Arrays.asList((byte)12, (byte)24, (byte)36,(byte)48,(byte)60));
-			Loan personal = new Loan("Personal", 100000, Arrays.asList((byte)6, (byte)12, (byte)24));
-			Loan automotive = new Loan("Automotive", 300000, Arrays.asList((byte)6, (byte)12, (byte)24, (byte)36));
-
-			ClientLoan clientLoan1 = new ClientLoan (400000, (byte)60);
-			ClientLoan clientLoan2 = new ClientLoan(50000, (byte)12);
-			ClientLoan clientLoan3 = new ClientLoan(100000, (byte)24);
-			ClientLoan clientLoan4 = new ClientLoan(200000, (byte)36);
-
-			Card card1 = new Card(returnCvvNumber(),randomNumberCard(cardRepository), CardType.DEBIT, CardColor.GOLD, LocalDate.now(), LocalDate.now().plusYears(5), client1);
-			Card card2 = new Card(returnCvvNumber(),randomNumberCard(cardRepository), CardType.CREDIT, CardColor.TITANIUM,LocalDate.now(), LocalDate.now().plusYears(5).plusMonths(2), client1);
-			Card card3 = new Card(returnCvvNumber(),randomNumberCard(cardRepository), CardType.CREDIT, CardColor.SILVER,LocalDate.now(), LocalDate.now().plusYears(5), client2);
-
-			client1.addAccount(account1);
-			client1.addAccount(account2);
-			client2.addAccount(account3);
-
-			client1.addCard(card1);
-			client1.addCard(card2);
-			client2.addCard(card3);
-
-			account1.addTransaction(transaction1);
-			account1.addTransaction(transaction2);
-			account2.addTransaction(transaction3);
-			account3.addTransaction(transaction4);
-
-			client1.addClientLoan(clientLoan1);
-			client1.addClientLoan(clientLoan2);
-			client2.addClientLoan(clientLoan3);
-			client2.addClientLoan(clientLoan4);
-
-			mortgage.addClientLoan(clientLoan1);
-			personal.addClientLoan(clientLoan2);
-			personal.addClientLoan(clientLoan3);
-			automotive.addClientLoan(clientLoan4);
-
-			clientRepository.saveAll(List.of(client1,client2,client3));
-
-			accountRepository.saveAll(List.of(account1,account2,account3));
-
-			transactionRepository.saveAll(List.of(transaction1,transaction2,transaction3,transaction4));
-
-			loanRepository.saveAll(List.of(mortgage,personal,automotive));
-
-			clientLoanRepository.saveAll(List.of(clientLoan1,clientLoan2,clientLoan3,clientLoan4));
-
-			cardRepository.saveAll(List.of(card1,card2,card3));
-		};
-	}
+//	@Bean
+//	public CommandLineRunner initData(ClientRepository clientRepository,
+//									  AccountRepository accountRepository,
+//									  TransactionRepository transactionRepository,
+//									  LoanRepository loanRepository,
+//									  ClientLoanRepository clientLoanRepository,
+//									  CardRepository cardRepository){
+//		return args -> {
+//			Client client1 = new Client("Melba", "Morel", "melba@mindhub.com", passwordEncoder.encode("melba123"));
+//			Client client2 = new Client("Nicolas", "Agnello", "nicoagnello@mindhub.com",  passwordEncoder.encode("nico123"));
+//			Client client3 = new Client("admin", "admin", "admin@mindhub.com",  passwordEncoder.encode("admin"));
+//
+//			Account account1 = new Account("VIN001", LocalDateTime.now(), 5000);
+//			Account account2 = new Account("VIN002", LocalDateTime.now().plusDays(1), 7500 );
+//			Account account3 = new Account("VIN003", LocalDateTime.now().plusMonths(1), 10000);
+//
+//			Transaction transaction1 = new Transaction(LocalDateTime.now(), -2000.12, TransactionType.DEBIT, "First transaction");
+//			Transaction transaction2 = new Transaction(LocalDateTime.now(), 3000, TransactionType.CREDIT, "Second transaction");
+//			Transaction transaction3 = new Transaction(LocalDateTime.now(), 1000.5, TransactionType.CREDIT, "Third transaction");
+//			Transaction transaction4 = new Transaction(LocalDateTime.now(), -500, TransactionType.DEBIT, "Fourth transaction");
+//
+//			Loan mortgage = new Loan("Mortgage", 500000, Arrays.asList((byte)12, (byte)24, (byte)36,(byte)48,(byte)60));
+//			Loan personal = new Loan("Personal", 100000, Arrays.asList((byte)6, (byte)12, (byte)24));
+//			Loan automotive = new Loan("Automotive", 300000, Arrays.asList((byte)6, (byte)12, (byte)24, (byte)36));
+//
+//			ClientLoan clientLoan1 = new ClientLoan (400000, (byte)60);
+//			ClientLoan clientLoan2 = new ClientLoan(50000, (byte)12);
+//			ClientLoan clientLoan3 = new ClientLoan(100000, (byte)24);
+//			ClientLoan clientLoan4 = new ClientLoan(200000, (byte)36);
+//
+//			Card card1 = new Card(returnCvvNumber(),randomNumberCard(cardRepository), CardType.DEBIT, CardColor.GOLD, LocalDate.now(), LocalDate.now().plusYears(5), client1);
+//			Card card2 = new Card(returnCvvNumber(),randomNumberCard(cardRepository), CardType.CREDIT, CardColor.TITANIUM,LocalDate.now(), LocalDate.now().plusYears(5).plusMonths(2), client1);
+//			Card card3 = new Card(returnCvvNumber(),randomNumberCard(cardRepository), CardType.CREDIT, CardColor.SILVER,LocalDate.now(), LocalDate.now().plusYears(5), client2);
+//
+//			client1.addAccount(account1);
+//			client1.addAccount(account2);
+//			client2.addAccount(account3);
+//
+//			client1.addCard(card1);
+//			client1.addCard(card2);
+//			client2.addCard(card3);
+//
+//			account1.addTransaction(transaction1);
+//			account1.addTransaction(transaction2);
+//			account2.addTransaction(transaction3);
+//			account3.addTransaction(transaction4);
+//
+//			client1.addClientLoan(clientLoan1);
+//			client1.addClientLoan(clientLoan2);
+//			client2.addClientLoan(clientLoan3);
+//			client2.addClientLoan(clientLoan4);
+//
+//			mortgage.addClientLoan(clientLoan1);
+//			personal.addClientLoan(clientLoan2);
+//			personal.addClientLoan(clientLoan3);
+//			automotive.addClientLoan(clientLoan4);
+//
+//			clientRepository.saveAll(List.of(client1,client2,client3));
+//
+//			accountRepository.saveAll(List.of(account1,account2,account3));
+//
+//			transactionRepository.saveAll(List.of(transaction1,transaction2,transaction3,transaction4));
+//
+//			loanRepository.saveAll(List.of(mortgage,personal,automotive));
+//
+//			clientLoanRepository.saveAll(List.of(clientLoan1,clientLoan2,clientLoan3,clientLoan4));
+//
+//			cardRepository.saveAll(List.of(card1,card2,card3));
+//		};
+//	}
 
 }
